65b526eef3262c8288cdba73ad07d780b9051ae4
Salar Farzaneh <salar.farzaneh420@gmail.com>
2026-08-20 04:16:10 +0330

    Import CircleShape for use in MainActivity
    Added CircleShape import for future use.

--- app/src/main/java/com/salvia/aiz/MainActivity.kt
+++ app/src/main/java/com/salvia/aiz/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
